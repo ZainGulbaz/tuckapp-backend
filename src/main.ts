db1eda81bc9d21ae188ac247e84930b87c4ae221
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-02-14 12:26:58 +0500

    Added main.ts file

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import { join } from 'path';
+
+console.log(process.env.GLOBAL_PREFIX);
+
+async function main() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'client/uploads/driver/lisence'));
+  app.useStaticAssets(join(__dirname, '..', 'client/uploads/driver/truck'));
+  app.useStaticAssets(join(__dirname, '..', 'client/uploads/customer'));
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: false }));
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+}
+
+main();
